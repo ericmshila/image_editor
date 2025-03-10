b0d08b75fa480626c33007acd327ae3519e1deef
Eric Mshila <emshila19@gmail.com>
2025-03-10 14:42:07 +0300

    working on grey scale feature

--- ImageEditor/processor/image_editor.py
+++ ImageEditor/processor/image_editor.py
@@ -12,6 +12,19 @@ def home():
     return render_template('home.html')
 
 
+def get_file():
+     # ensure file upload
+    if 'file' not in request.files:
+        return {'error: No file uploaded'}, 400
+    file = request.files['file'] #retrieve file
+    image_bytes = file.read() # read contents 
+    return image_bytes
+
+def convert_greyscale(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.convert('L')
+    return image
+
 #IMAGE PROCESSOR
 def deblur_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes)) #opens the image as a BINARY STREAM as if it weas beinG read from a file
@@ -29,28 +42,25 @@ def deblur_image(image_bytes):
     deblurred = cv2.filter2D(image, -1, kernel) #applies kernel to image pixel by pixel. -1 =is desired depth of output
     return Image.fromarray(deblurred) #reverse of np.array(image). Creates PIL image from numpy array
 
-def convert_greyscale(image_bytes):
-    image = Image.open(io.BytesIO(image_bytes))
-    edit = image.convert('L')
-
+@app.route("/grayscale", methods = ["POST"])
+def greyscale():
+   
+    greyscale_image = convert_greyscale( get_file())
+    img_io = io.BytesIO()
+    greyscale_image.save(img_io, format = "PNG")
+    img_io.seek(0) #resets pointer so you can read the data from the start
+    return send_file(img_io, mimetype='image/png')
 
 
 @app.route("/deblur", methods=["POST"])
 #API HANDLER
 def deblur():
-    # ensure file upload
-    if 'file' not in request.files:
-        return {'error: No file uploaded'}, 400
     
-    file = request.files['file'] #retrieve file
-    image_bytes = file.read() # read contents 
-    deblurred_image = deblur_image(image_bytes) # call deblur image function
+    deblurred_image = deblur_image(get_file()) # call deblur image function
     img_io = io.BytesIO() #shorten the io.BytesIO class. Allows you to use im_memory_file as a file object
-    deblurred_image.save(img_io, format="PNG")
+    deblurred_image.save(img_io, format="PNG") # save image in PNG format
     img_io.seek(0) #resets pointer so you can read the data from the start
     return send_file(img_io, mimetype='image/png') #sends response to client
-
-
 
 
 
